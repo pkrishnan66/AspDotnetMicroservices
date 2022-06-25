0ff0be61d2541aef3bace6a483610d91d8b128d3
pkrishnan66@gmail.com <pkrishnan66@gmail.com>
2022-06-25 12:10:36 +1000

    Adding GRPC and Rabbit Mq

---
+++ src/services/discount/Discount.Grpc/Repositories/DiscountRepository.cs
@@ -0,0 +1,40 @@
+﻿using Discount.Grpc.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace Discount.Grpc.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Coupon> GetDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
